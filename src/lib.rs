dc51ff9dde3828ac2d4af1d59a9ef154c97565d1
WKHAllen <wkhallen@gmail.com>
2024-07-28 13:26:36 -0400

    Document all items

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+/// The `Pipe` trait creates a functional pipe by wrapping operations within one
+/// another. Each call to the `pipe` method creates another wrapper and returns
+/// the resulting function.
+///
+/// When in scope, this trait is implemented for all types implementing
+/// `FnOnce(A) -> B`, for any types `A` and `B`.
 pub trait Pipe<A, B, C> {
+    /// Wraps the provided function or closure inside the currently constructed
+    /// pipeline. See the documentation for the [`pipe`] function for examples.
     fn pipe<F>(self, f: F) -> impl FnOnce(A) -> C
     where
         F: FnOnce(B) -> C;
@@ -16,6 +24,21 @@ where
     }
 }
 
+/// This is a convenience function to start a pipeline.
+///
+/// The compiler often has difficulty inferring pipe input types, so it is
+/// usually a good idea to explicitly provide the input type when using this
+/// function.
+///
+/// ```
+/// # use pipe::*;
+/// let remove_long_words = pipe(|s: &str| s.split(' '))
+///     .pipe(|split| split.filter(|s| s.len() <= 4))
+///     .pipe(|filtered| filtered.collect::<Vec<_>>())
+///     .pipe(|words| words.join(" "));
+/// let short_words = remove_long_words("foo bar hello world baz");
+/// assert_eq!(short_words, "foo bar baz");
+/// ```
 pub fn pipe<F, A, B>(f: F) -> impl FnOnce(A) -> B
 where
     F: FnOnce(A) -> B,
@@ -23,14 +46,31 @@ where
     f
 }
 
+/// The `pipe` macro provides an alternative syntax for constructing pipelines.
+///
+/// The macro syntax is as follows:
+/// `<input identifier>: <first input type>; <pipe expression> => <pipe expression> => ... => <pipe expression>`
+///
+/// ```
+/// # use pipe::*;
+/// let remove_long_words = pipe! { this: &str;
+///        this.split(' ')
+///     => this.filter(|s| s.len() <= 4)
+///     => this.collect::<Vec<_>>()
+///     => this.join(" ")
+/// };
+/// let short_words = remove_long_words("foo bar hello world baz");
+/// assert_eq!(short_words, "foo bar baz");
+/// ```
 #[macro_export]
 macro_rules! pipe {
-    ( $ident:ident: $ty:ty; $first:expr => $( $rest:expr )=>* ) => {
-        pipe(|$ident: $ty| $first)
+    ( $ident:ident: $ty:ty; $first:expr => $( $rest:expr )=>* ) => {{
+        use $crate::Pipe;
+        $crate::pipe(|$ident: $ty| $first)
         $(
             .pipe(|$ident| $rest)
         )+
-    };
+    }};
 }
 
 #[cfg(test)]
